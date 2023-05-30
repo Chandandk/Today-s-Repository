b221410fcf0cdc88f6e90d5e6b1a29c42ea4f2bb
Chandan kumar <85033367+Chandandk@users.noreply.github.com>
2023-05-30 20:20:40 +0530

    Update bulb_on_off.js

--- bulb_on_off.js
+++ bulb_on_off.js
@@ -3,3 +3,7 @@ function bulbOn(){
     let obj=document.querySelector(".bulboff");
     obj.setAttribute("scr","img/bulb_on.png");
 }
+function bulbOff(){
+    let obj=document.querySelector(".bulboff");
+    obj.setAttribute("scr","img/bulb_off.png");
+}
